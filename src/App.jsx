7c3b79c65fd3f10cd9ad815ffcae3be044648b41
Rahul Bisht <bishtrahul6400@gmail.com>
2025-03-12 12:49:20 +0530

    Project planning and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-
 function App() {
   return (
     <>
-      <h3>Hello Guys! Lets build Netflix-GPT</h3>
+      <h3 className="text-3xl font-bold underline text-red-400" >Hello Guys! Lets build Netflix-GPT</h3>
+      <p className='text-3xl font-bold'>Rahul Bisht</p>
     </>
   )
 }
