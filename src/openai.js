240a9a81f3e317a5e1ce0ac95c4eac32263daa73
Rahul Bisht <bishtrahul6400@gmail.com>
2025-03-19 23:37:18 +0530

    working on search functionality and currently stucked on gpt part

--- src/openai.js
+++
@@ -1,9 +0,0 @@
-import OpenAI from "openai";
-import { OPENAI_KEY } from "./utils/constants";
-
-const openai = new OpenAI({
-    apiKey:OPENAI_KEY,
-    dangerouslyAllowBrowser: true,
-})
-
-export default openai;
