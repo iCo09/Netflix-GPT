dbefce33bed2832c32e91c1bfb157009b88af899
Rahul Bisht <bishtrahul6400@gmail.com>
2025-03-20 00:03:52 +0530

    Restored constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const LOGO = "https://imgs.search.brave.com/pB7rmF0nJIRrC9TeIkUEjCoOrOx8oh-ZcEy8Qci1QgA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ODBiNTdmY2Q5OTk2/ZTI0YmM0M2M1Mjku/cG5n";
+export const USER_AVATAR = "https://avatars.githubusercontent.com/u/97499344?v=4"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmY0YjQ2ZTc4ODBmMWUzOGI1ZWYzMzMzMzRhNjljYSIsIm5iZiI6MTc0MTk3NTcyNi42NjksInN1YiI6IjY3ZDQ3MGFlZTllNTg0NmEzZjAyOTI4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IWKkBgt7iGhjUMKw-TfcVOQmkKBgD71835kBf9q-3GA'
+    }
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
