b248ef52cb760a43e8b476468186cd244a6066b5
Rahul Bisht <bishtrahul6400@gmail.com>
2025-03-20 23:50:00 +0530

    Put the TMDB API KEY in env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,12 +2,13 @@ export const LOGO = "https://imgs.search.brave.com/pB7rmF0nJIRrC9TeIkUEjCoOrOx8o
 export const USER_AVATAR = "https://avatars.githubusercontent.com/u/97499344?v=4"
 
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmY0YjQ2ZTc4ODBmMWUzOGI1ZWYzMzMzMzRhNjljYSIsIm5iZiI6MTc0MTk3NTcyNi42NjksInN1YiI6IjY3ZDQ3MGFlZTllNTg0NmEzZjAyOTI4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IWKkBgt7iGhjUMKw-TfcVOQmkKBgD71835kBf9q-3GA'
-    }
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}` // Corrected syntax with backticks
+  }
 };
+
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
